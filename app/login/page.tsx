4d1b2c6689c9ea68295f87b14e535b39b3b99b46
adhitstrife63 <adhitstrife63@gmail.com>
2025-01-14 06:00:43 +0000

    under construction page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,60 @@
 "use client"
 import { Box, Button, Card, Center, Flex, Image, Loader, PasswordInput, Text, TextInput, Title } from '@mantine/core';
+import { ColorSchemeToggle } from '../../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Welcome } from '../../components/Welcome/Welcome';
+import { useViewportSize } from '@mantine/hooks';
 import { useMantineTheme } from '@mantine/core';
 import Link from 'next/link';
 import useLogin from '@/hooks/auth/useLogin';
 import React, { useState } from 'react';
-import style from '@/app/login/style/bd-coming-soon.module.css'
 
 
 export default function HomePage() {
+  const theme = useMantineTheme();
+  const { height, width } = useViewportSize();
+  const { login, isLoading } = useLogin();
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setCredentials({
+      ...credentials,
+      [name]: value
+    });
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await login(credentials);
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
   return (
-    <div className="">
-     
-    </div>
+    <Flex direction={"column"} mih={height} px={20}>
+      <Box mt={100} className="">
+        <Image mx={"auto"} w={200} src={"/logo.png"} />
+      </Box>
+      <div className="title">
+        <Title mt={112} ta={"center"} order={2}>Welcome</Title>
+        <Text ta={"center"} style={{ color: theme.colors.gray[5] }}>Continue sign in to started the app</Text>
+      </div>
+      <Center mt={30} className="input">
+        <form onSubmit={handleSubmit}>
+          <Flex direction={'column'} w={{ base: 300, sm: 355 }}>
+            <TextInput size='md' placeholder='Username' name='username' onChange={(e) => handleChange(e)} />
+            <PasswordInput mt={20} size='md' name='password' onChange={(e) => handleChange(e)} placeholder='Password' />
+            <Text href={"/dashboard"} component={Link} ta={"right"} mt={12} c={"blue"} size='sm'>Forgot Password</Text>
+            <Button type='submit' variant='filled' mt={66} color={"primary-red"}>{isLoading ? <Loader color='white' size={'sm'} /> : 'Sign In'}</Button>
+          </Flex>
+        </form>
+      </Center>
+    </Flex>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,53 @@
 "use client"
 import { Box, Button, Card, Center, Flex, Image, Loader, PasswordInput, Text, TextInput, Title } from '@mantine/core';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
-import { useViewportSize } from '@mantine/hooks';
 import { useMantineTheme } from '@mantine/core';
 import Link from 'next/link';
 import useLogin from '@/hooks/auth/useLogin';
 import React, { useState } from 'react';
+import style from '@/app/style/bd-coming-soon.module.css'
 
 
 export default function HomePage() {
-  const theme = useMantineTheme();
-  const { height, width } = useViewportSize();
-  const { login, isLoading } = useLogin();
-  const [credentials, setCredentials] = useState({
-    username: '',
-    password: ''
-  })
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
-    setCredentials({
-      ...credentials,
-      [name]: value
-    });
-  }
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      await login(credentials);
-    } catch (error) {
-      console.error("Login failed:", error);
-    }
-  };
-
   return (
-    <Flex direction={"column"} mih={height} px={20}>
-      <Box mt={100} className="">
-        <Image mx={"auto"} w={200} src={"/logo.png"} />
-      </Box>
-      <div className="title">
-        <Title mt={112} ta={"center"} order={2}>Welcome</Title>
-        <Text ta={"center"} style={{ color: theme.colors.gray[5] }}>Continue sign in to started the app</Text>
+    <div className="">
+      <style jsx global>{`
+        * {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+        }
+        html, body {
+          height: 100%;
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+
+        body {
+          font-size: 110%;
+          color: #ecf0f1;
+          font-family: 'Source Sans Pro', sans-serif;
+          background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+          width: 100%;
+          position: absolute;
+          top: 0;
+          left: 0;
+          overflow: hidden;
+        }
+        
+      `}</style>
+      <div className="">
+        <div className={style.overlay}></div>
+        <div className={style.stars} aria-hidden="true"></div>
+        <div className={style.starts2} aria-hidden="true"></div>
+        <div className={style.stars3} aria-hidden="true"></div>
+        <main className={style.main}>
+          <section className={style.contact}>
+            <h1 className={style.title}>Awesome Thing</h1>
+            <h2 className={style.subTitle}>Site Under Construction</h2>
+          </section>
+        </main>
       </div>
-      <Center mt={30} className="input">
-        <form onSubmit={handleSubmit}>
-          <Flex direction={'column'} w={{ base: 300, sm: 355 }}>
-            <TextInput size='md' placeholder='Username' name='username' onChange={(e) => handleChange(e)} />
-            <PasswordInput mt={20} size='md' name='password' onChange={(e) => handleChange(e)} placeholder='Password' />
-            <Text href={"/dashboard"} component={Link} ta={"right"} mt={12} c={"blue"} size='sm'>Forgot Password</Text>
-            <Button type='submit' variant='filled' mt={66} color={"primary-red"}>{isLoading ? <Loader color='white' size={'sm'} /> : 'Sign In'}</Button>
-          </Flex>
-        </form>
-      </Center>
-    </Flex>
+    </div>
   );
 }
